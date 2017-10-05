61b4df19d391ab5be33c35187388232e7b56b279
enderbie <s.patotski@gmail.com>
2017-10-05 01:31:55 -0400

    Script that collects the data from arxiv

---
+++ cornetto/scripts/harvest_arxiv.py
@@ -0,0 +1,45 @@
+"""
+This script collects the data from arxiv.org. Collects metadata of all 
+the papers published between two given days 
+(both days given as YYYY-MM-DD strings). The metadata we collect is:
+
+  -- 'arXiv_id' , unique id, of the format 'int.int' or just 'int'
+  -- 'Title' , the title of the paper
+  -- 'Authors' , as a string of last and first names separated by a delimiter 
+  -- 'Date' , date of publication, YYYY-MM-DD
+  -- 'MSCs' , string of 5-character codes, separated by a delimiter. These 
+  codes are given by the standart classification of math subjects, 
+  see https://zbmath.org/classification/
+  -- 'Abstract' , short description of the content of the paper
+
+The data is saved as pandas DataFrame, 
+by default into the folder '../data/arxiv/'
+"""
+
+import datetime        
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from modules.arxiv_harvester import ArxivHarvester
+
+def validate(date_text):
+    try:
+        datetime.datetime.strptime(date_text, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError("Date should be in the form YYYY-MM-DD")
+
+if __name__ == '__main__':
+    print("Harvesting arxiv.org. Note: this process takes a while, ")
+    print("because of the necessary pause between server requests. ")
+    print("--------------------------------------------------------")
+    print("Enter the start date as YYYY-MM-DD. Default is 2010-01-01.")
+    print("Just press enter to use the default.")
+    start_date = input()
+    print("Enter the end date as YYYY-MM-DD. Default is today.")
+    print("Just press enter to use the default.")
+    end_date = input()
+    
+    print("Starting the harvest...")
+    harvester = ArxivHarvester()
+    harvester.harvest(start_date=start_date, end_date=end_date)
+    print("The harvest was successful!")
